4127b7587ebaaef2978fddb2ce5877c98d9c1476
gowtham <valmeeki1991@gmail.com>
2017-10-11 13:42:51 +0000

    modified changes

--- Integers.java
+++ Integers.java
@@ -5,6 +5,7 @@ class Integers {
   /* Using for loop to repeat instruction execution */
  
     for (c = 1; c <= 10; c++) {
+	System.out.println("Numbers");
       System.out.println(c);
     }
   }
